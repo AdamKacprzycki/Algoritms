9da54465c1e2238a833b11e66aea454a21f85b71
Adam Kacprzycki <adamkacprzycki@gmail.com>
2022-04-11 13:08:42 +0200

    Additional small chnges in code eg. comments ect

--- Binary_Search.py
+++ Binary_Search.py
@@ -12,7 +12,7 @@ def binary_search(num_list_sorted, num_to_find):
         guess = num_list_sorted[mid_index]
 
         # Steps in loop, to check results.
-        # print("Step #{0}, min_index:{1}, max_index: {2}, mid_index: {3}, guess: {4}". \
+        # print("Step #{0}, min_index:{1}, max_index: {2}, mid_index: {3}, guess: {4}".
         #       format(step, min_index, max_index, mid_index, guess))
 
         # If number to find is equal to guessed number than return no of loop steps and position index (tuple)
@@ -36,6 +36,6 @@ list_b = range(1, 100)
 find_number = 20
 
 # In general there is way to define max number of steps to find number using binary search as log(number, log base=2).
-print("Maximum steps in binary search: {0}, steps of algorithm: {1}, number was found under no of index: {2}.". \
+print("Maximum steps in binary search: {0}, steps of algorithm: {1}, number was found under no of index: {2}.".
       format(math.ceil(math.log(len(list_b), 2)), binary_search(list_b, find_number)[0],
              binary_search(list_b, find_number)[1]))
